a60e7c8ef16d0bcd0d54eb4ce3d33e814db419b8
codeandb <andersonphang001@gmail.com>
2025-04-04 19:07:00 +0700

    Changes to be committed: 	modified:   configuration.nix 	modified:   home.nix

--- home.nix
+++ home.nix
@@ -1,49 +1,65 @@
-  { config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.username = "anderson";
+  home.homeDirectory = "/home/anderson";
 
-  {
-   home.username = "anderson";
-   home.homeDirectory = "/home/anderson";
-   
-   # User Packages
-   home.packages = with pkgs; [
-     screenfetch
-     ranger
-     zip
-     xz
-     unzip 
-     p7zip
-     eza
-     tree
-     gnutar
-     btop
-     pavucontrol
-     nwg-look
-     nerdfonts
-     waybar
-     grim
-     slurp
-     git-credential-manager
-     vscode
-     nix-prefetch-git
-   ];
-   
-   programs.fish.enable = true;
-   programs.fish.shellAliases = {
-   ls = "eza -al --group-directories-first --icons";
-   vim = "nvim";
-   };
-   
-   # Git 
-   programs.git = {
-     enable = true;
-     userName = "codeandb";
-     userEmail = "andersonphang001@gmail.com";
-     extraConfig.credential.helper = "manager";
-     extraConfig.credential."https://github.com".username = "codeandb";
-     extraConfig.credential.credentialStore = "cache"; 
-   };
+  # User Packages
+  home.packages = with pkgs; [
+    screenfetch
+    ranger
+    zip
+    xz
+    unzip
+    p7zip
+    eza
+    tree
+    gnutar
+    btop
+    pavucontrol
+    nwg-look
+    nerdfonts
+    waybar
+    grim
+    slurp
+    git-credential-manager
+    vscode
+    nix-prefetch-git
+    adwaita-icon-theme
+    tela-icon-theme
+    adw-gtk3
+  ];
 
-   home.stateVersion = "24.11";
-   programs.home-manager.enable = true;
-  }
+  programs.fish.enable = true;
+  programs.fish.shellAliases = {
+    ls = "eza -al --group-directories-first --icons";
+    vim = "nvim";
+  };
 
+  # Theming
+  gtk.enable = true;
+
+  #gtk.cursorTheme.package = pkgs.bibata-cursors;
+  #gtk.cursorTheme.name = "Bibata-Modern-Ice";
+
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3";
+
+  gtk.iconTheme.package = pkgs.tela-icon-theme;
+  gtk.iconTheme.name = "Tela-black-dark";
+
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "codeandb";
+    userEmail = "andersonphang001@gmail.com";
+    extraConfig.credential.helper = "manager";
+    extraConfig.credential."https://github.com".username = "codeandb";
+    extraConfig.credential.credentialStore = "cache";
+  };
+
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}
